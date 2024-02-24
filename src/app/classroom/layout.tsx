b9a953c3ceb74494c9cd787d42b6abb2ddba4ac3
User <sanseb290514@gmail.com>
2024-02-23 21:35:44 -0500

    fix: add missing import for file

--- src/app/classroom/layout.tsx
+++ src/app/classroom/layout.tsx
@@ -4,12 +4,12 @@ import { Authenticator } from "@aws-amplify/ui-react";
 import '@aws-amplify/ui-react/styles.css';
 
 import { Amplify } from 'aws-amplify'
-import awsExports from "../../aws-exports";
-import React from "react";
+import awsExports from "src/aws-exports";
+import {ReactNode} from "react";
 Amplify.configure(awsExports);
 
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
     return (
         <Authenticator>
             {
