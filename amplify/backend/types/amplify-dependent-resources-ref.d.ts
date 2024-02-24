0cd5919ec818bbeb423cd4b9b9a3bb874d196ce6
User <sanseb290514@gmail.com>
2024-02-23 21:12:26 -0500

    Feat: implement AWS authentication
    Added basic authentication functionality using AWS Cognito and their own components

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "front9e05b2ab": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/app/classroom/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { Authenticator } from "@aws-amplify/ui-react";
+import '@aws-amplify/ui-react/styles.css';
+
+import { Amplify } from 'aws-amplify'
+import awsExports from "../../aws-exports";
+Amplify.configure(awsExports);
+
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+    return (
+        <Authenticator>
+            {
+                children
+            }
+        </Authenticator>
+    )
+}
