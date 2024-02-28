a3203e4364491c69513b8fdc8a973c3c3a50d19e
User <sanseb290514@gmail.com>
2024-02-28 09:59:17 -0500

    feat: add styles to authentication forms

--- src/app/classroom/layout.tsx
+++ src/app/classroom/layout.tsx
@@ -1,22 +1,31 @@
 "use client";
+import { ReactNode } from "react";
 
-import { Authenticator, Button, Heading, Image, Text, View, useAuthenticator, useTheme, } from "@aws-amplify/ui-react";
-import '@aws-amplify/ui-react/styles.css';
-
+import { Authenticator, Image } from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify'
 import awsExports from "src/aws-exports";
-import { ReactNode } from "react";
 import { DefaultComponents } from "node_modules/@aws-amplify/ui-react/dist/types/components/Authenticator/hooks/useCustomComponents/defaultComponents";
+import { I18n } from 'aws-amplify/utils';
+import { translations } from '@aws-amplify/ui-react';
+
+
+I18n.putVocabularies(translations);
+I18n.setLanguage('es');
 Amplify.configure(awsExports);
 
 
 export default function Layout({ children }: { children: ReactNode }) {
     return (
-        <Authenticator className="auth-layout">
-            {
-                children
-            }
-        </Authenticator>
+        <Authenticator className="auth-layout" components={components}>{children}</Authenticator>
     )
 }
 
+const components: DefaultComponents = {
+    Header() {
+        return (
+            <div className="amplify-custom-header">
+                <Image src="/images/pragma-logo-long.png" alt="Pragma logo" maxWidth={124} />
+            </div>
+        )
+    },
+};
